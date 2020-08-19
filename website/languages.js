c9c3eadb29d46f045341097f88627279a3180b35
HarshadRanganathan <rharshad93@gmail.com>
2020-08-19 22:33:37 +0100

    More languages support

--- website/languages.js
+++ website/languages.js
@@ -11,13 +11,53 @@ const languages = [
   },
   {
     enabled: true,
-    name: '简体中文',
-    tag: 'zh-CN',
+    name: 'Deutsch',
+    tag: 'de-DE',
+  },
+  {
+    enabled: true,
+    name: 'Español',
+    tag: 'es-ES',
   },
   {
     enabled: true,
     name: 'Français',
     tag: 'fr-FR',
-  }
+  },
+  {
+    enabled: true,
+    name: 'Italiano',
+    tag: 'it-IT',
+  },
+  {
+    enabled: true,
+    name: '日本語',
+    tag: 'ja-JP',
+  },
+  {
+    enabled: true,
+    name: 'Polskie',
+    tag: 'pl-PL',
+  },
+  {
+    enabled: true,
+    name: 'Português (Brasil)',
+    tag: 'pt-BR',
+  },
+  {
+    enabled: true,
+    name: 'Română',
+    tag: 'ro-RO',
+  },
+  {
+    enabled: true,
+    name: 'Русский',
+    tag: 'ru-RU',
+  },
+  {
+    enabled: true,
+    name: '简体中文',
+    tag: 'zh-CN',
+  },
 ];
 module.exports = languages;
