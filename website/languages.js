8a0f6a9d9ee3f32cc77b4541bbb87d8edb490d1b
HarshadRanganathan <rharshad93@gmail.com>
2020-08-19 22:11:22 +0100

    Add language support

---
+++ website/languages.js
@@ -0,0 +1,23 @@
+const languages = [
+  {
+    enabled: true,
+    name: 'English',
+    tag: 'en',
+  },
+  {
+    enabled: true,
+    name: 'العربية',
+    tag: 'ar-SA',
+  },
+  {
+    enabled: true,
+    name: '简体中文',
+    tag: 'zh-CN',
+  },
+  {
+    enabled: true,
+    name: 'Français',
+    tag: 'fr-FR',
+  }
+];
+module.exports = languages;
