62a1662ff334c212b01c099de407bde3a2fd39a3
HarshadRanganathan <rharshad93@gmail.com>
2022-08-21 23:38:09 +0100

    FIx base url

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -2,7 +2,7 @@ module.exports={
   "title": "Ireland Immigration",
   "tagline": "Your immigration, visa and work permit queries answered by the community",
   "url": "https://harshadranganathan.github.io",
-  "baseUrl": "/",
+  "baseUrl": "/ireland-immigration-board/",
   "organizationName": "harshadranganathan",
   "projectName": "ireland-immigration-board",
   "scripts": [
